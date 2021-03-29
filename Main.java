c64a56f88f5c07e8634b3405dd3f4bec99d0dac0
Lauraine Baffot <74086642+Lauraine2112@users.noreply.github.com>
2021-03-29 00:26:23 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 package edu.ucalgary.ensf409;
-
 import java.util.*;
 import java.sql.*;
 import java.lang.*;
 
 public class Main {
-
 	public static void main(String[] args) {
 //		Scanner myUser = new Scanner(System.in); //Create scanner object
 //		System.out.println("Please input username: "); //prompt input from user
@@ -24,40 +22,34 @@ public class Main {
 //		String numItems = myUser.nextLine();  //Read user input
 		
 		
-		LocateRequest myJDBC = new LocateRequest("jdbc:mysql://localhost/inventory", "lauraine", "ensf409", "desk", "Traditional", 2);
+		LocateRequest myJDBC = new LocateRequest("jdbc:mysql://localhost/inventory", "lauraine", "ensf409", "chair", "Mesh", 1);
 		myJDBC.initializeConnection();
 		
 		myJDBC.getRequest();
-		System.out.println("The best price is: " + myJDBC.getBestPrice());
-		System.out.println("The total price is: " + myJDBC.getTotalPrice());
+//		System.out.println("The best price is: " + myJDBC.getBestPrice());
+//		System.out.println("The total price is: " + myJDBC.getTotalPrice());
 //		System.out.print("The best combination is:");
 //		for (String val: myJDBC.getBestCombination()){
 //			System.out.print(" " + val);
 //		}
 		//System.out.println(Arrays.toString(row));
-		System.out.println();
-		System.out.print("The orderedIds is:");
-		for (String val: myJDBC.getOrderedIDs()){
-			System.out.print(" " + val);
-		}
-
-//		for (String[] inner: myJDBC.getAllCombination()){
-//			for(String val: inner) {
-//				System.out.print(val);
-//			}
-//			System.out.println();
+//		System.out.println();
+//		System.out.print("The orderedIds is:");
+//		for (String val: myJDBC.getOrderedIDs()){
+//			System.out.print(" " + val);
 //		}
 
-		// for (String[] inner: myJDBC.getRequest()){
-		// 	for(String val: inner){
-		// 		System.out.print(val);
-		// 	}
-		// 	System.out.println();
-		// }
-		
-		
-		
+//		for (String[] inner: myJDBC.getFoundRequest()){
+//			System.out.println(Arrays.toString(inner));
+//		}
+
+//		 for (String[] inner: myJDBC.getRequest()){
+//		 	for(String val: inner){
+//		 		System.out.print(val);
+//		 	}
+//		 	System.out.println();
+//		 }
+
 		myJDBC.close();
 	}
-
 }
