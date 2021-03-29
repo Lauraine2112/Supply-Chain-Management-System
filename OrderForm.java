7b58cdef762c781de3392afc16cb04abab6bf296
Lauraine Baffot <74086642+Lauraine2112@users.noreply.github.com>
2021-03-29 00:26:49 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -1,44 +1,41 @@
 package edu.ucalgary.ensf409;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.io.FileWriter;
 
 public class OrderForm {
-//	 public void createFile(){
-//		 try {
-//		 File myObj = new File("orderForm.txt");
-//		 if (myObj.createNewFile()) {
-//		 System.out.println("File created: " + myObj.getName());
-//		 } else {
-//		 System.out.println("File already exists.");
-//		 }
-//		 } catch (IOException e) {
-//		 System.out.println("An error occurred.");
-//		 e.printStackTrace();
-//		 }
-//
-//		 try {
-//		 FileWriter myWriter = new FileWriter("filename.txt");
-//		 myWriter.write("Furniture Order Form/n/n/nFacultyName:/nContact:/nDate:/n/n");
-//		 myWriter.write("Original Request: "+ type + " " + furniture + ", " + amount);
-//		 myWriter.write("Items Ordered/nID: " );
-//		 myWriter.write("ID: " );
-//		 myWriter.close();
-//		 System.out.println("Successfully wrote to the file.");
-//		 } catch (IOException e) {
-//		 System.out.println("An error occurred.");
-//		 e.printStackTrace();
-//		 }
-//
-//		 }
-//
-//		 public void printMessage(){
-//			 if(requestStatus == True){
-//				 System.out.println("User request: "+ type + furniture + ", " + amount);
-//				 System.out.println("Purchase" + something + " and " + something +" for " + lowestPrice);
-//			 }
-//			 else if(requestStatus == False){
-//				 System.out.println("Order cannot be fulfilled base on the current inventory. Suggested manufacturers are" + ); 
-//			 }
-//		 }
+	 public void createFile() {
+		 try {
+			 File myObj = new File("orderForm.txt");
+			 
+			 if (myObj.createNewFile()) {
+				 System.out.println("File created: " + myObj.getName());
+			 	} else {
+			 		System.out.println("File already exists or does not exist.");
+			 	}
+		 } catch (IOException e) {
+			 e.printStackTrace();
+		 }
+		 
+		 try {
+			 String[] unique = Arrays.stream(LocateRequest.getOrderedIDs()).distinct().toArray(String[]::new);
+			 FileWriter myWriter = new FileWriter("orderForm.txt");
+			 
+			 myWriter.write("Furniture Order Form\n\nFacultyName:\nContact:\nDate:\n\n");
+			 myWriter.write("Original Request: " + LocateRequest.getType() + " " 
+					 		+ LocateRequest.getFurniture() + ", " 
+					 		+ LocateRequest.getAmount());
+			 myWriter.write("\n\nItems Ordered\n" );
+			 for (String val : unique) {
+					myWriter.write("ID: " + val + "\n");
+				}
+			 myWriter.write("\nTotal Price: $" + LocateRequest.getTotalPrice() );
+			 myWriter.close();
+			 
+			 System.out.println("Successfully wrote to the file.");
+		 } catch (IOException e) {
+			 e.printStackTrace();	 
+		 }
+	 }
 }
